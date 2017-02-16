8a4c860a464d50585761899be03625f62e29f0e5
LOlena <1695208@bdeb.qc.ca>
2017-02-16 00:07:53 -0500

    Add Name to User Identity

--- MesTaches/MesTaches/Models/ManageViewModels.cs
+++ MesTaches/MesTaches/Models/ManageViewModels.cs
@@ -12,6 +12,7 @@ namespace MesTaches.Models
         public string PhoneNumber { get; set; }
         public bool TwoFactor { get; set; }
         public bool BrowserRemembered { get; set; }
+        public string Name { get; set; }
     }
 
     public class ManageLoginsViewModel
